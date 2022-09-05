051f93ba433bc34163fd2919c725017a06b04422
Elena Balab <elena.mb.711@gmail.com>
2022-09-05 21:55:38 +0300

    Add form validation

--- src/index.js
+++ src/index.js
@@ -1,78 +1,67 @@
 import 'bootstrap';
-import { object, string } from 'yup';
+import { string } from 'yup';
 import onChange from 'on-change';
+import render from './view.js';
 import './style.scss';
 
-const form = document.querySelector('form');
-const input = document.querySelector('#url-input');
-const feedback = document.querySelector('.feedback');
+const app = () => {
+  const form = document.querySelector('form');
+  const input = document.querySelector('#url-input');
 
-const state = {
-  input: {
-    value: '',
-    isValid: true,
-    errors: [],
-  },
-  feedList: [],
-};
-
-const watchedState = onChange(state, (path, value) => {
-  if (path === 'input.isValid') {
-    if (value === false) {
-      input.classList.add('is-invalid');
-    } else {
-      input.classList.remove('is-invalid');
-    }
-  } if (path === 'input.errors') {
-    feedback.classList.remove('text-success');
-    feedback.classList.add('text-danger');
-    feedback.textContent = value;
-  } if (path === 'feedList') {
-    feedback.classList.remove('text-danger');
-    feedback.classList.add('text-success');
-    feedback.textContent = 'RSS успешно загружен';
-  }
-});
-
-const urlSchema = object({
-  url: string().url('Ссылка должна быть валидным URL').nullable().test({
-    name: 'feed-already-added',
-    skipAbsent: true,
-    test(value, ctx) {
-      if (state.feedList.includes(value)) {
-        return ctx.createError({ message: 'RSS уже существует' });
-      }
-      return true;
+  const state = {
+    input: {
+      value: '',
+      isValid: true,
+      error: {},
+      feedbackMessage: '',
     },
-  }),
-});
+    feedList: [],
+  };
 
-input.addEventListener('input', (e) => {
-  const { value } = e.target;
-  watchedState.input.value = value;
-});
-
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const { value } = state.input;
-  const promise = new Promise((resolve) => {
-    resolve(urlSchema.validate({ url: value }));
+  const watchedState = onChange(state, () => {
+    render(state);
   });
-  promise.then(({ url }) => {
+
+  const handleInputChange = ({ target: { value } }) => {
+    watchedState.input.value = value;
+  };
+
+  const handleSuccess = (url) => {
     watchedState.input.value = '';
     watchedState.input.isValid = true;
-    watchedState.input.errors = [];
+    watchedState.input.error = {};
+    watchedState.input.feedbackMessage = 'RSS успешно загружен';
     watchedState.feedList.push(url);
-    // console.log(state);
-    input.value = watchedState.input.value;
-    input.focus();
-  })
-    .catch((err) => {
-      console.log('error!');
-      watchedState.input.isValid = false;
-      watchedState.input.errors = err.message;
-      // console.log(state);
+  };
+
+  const handleError = (err) => {
+    watchedState.input.isValid = false;
+    watchedState.input.error = err;
+    watchedState.input.feedbackMessage = err.message;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { feedList, input: { value } } = state;
+
+    const validate = (str) => new Promise((resolve) => {
+      const urlScema = string()
+        .url('Ссылка должна быть валидным URL')
+        .notOneOf(feedList, 'RSS уже существует');
+
+      resolve(urlScema.validate(str));
     });
-  // console.log('submit:', promise);
-});
-// console.log('hi from js!');
+
+    validate(value).then((url) => {
+      handleSuccess(url);
+    }).catch((e) => {
+      handleError(e);
+    });
+  };
+
+  input.addEventListener('input', handleInputChange);
+
+  form.addEventListener('submit', handleSubmit);
+};
+
+app();
