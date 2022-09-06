cb936d9b0e0c42a4a3dc352836496b266d77f221
Elena Balab <elena.mb.711@gmail.com>
2022-09-06 13:05:05 +0300

    Add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const INPUT_ERR_INVALID = 'Ссылка должна быть валидным URL';
+
+export const INPUT_ERR_CONFLICT = 'RSS уже существует';
+
+export const INPUT_SUCCESS = 'RSS успешно загружен';
