7c55814e6a1f6d6204ca4452e1d436a87eb12a73
Elena Balab <elena.mb.711@gmail.com>
2022-09-22 20:43:48 +0300

    Fix bug

--- src/fetchFeed.js
+++ src/fetchFeed.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 export default (uri) => (axios
   .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(uri)}`)
   .then(({ data }) => {
-    const { contents, status: { url, content_type: contentType } } = data;
+    const { contents, status: { content_type: contentType } } = data;
     if (!contentType.includes('application/rss+xml')) {
       const error = {
         errors: [{ key: 'ERR_INVALID_RSS' }],
       };
       throw error;
     }
-    return { url, contents };
+    return { url: uri, contents };
   }))
   .catch(() => {
     const error = {
