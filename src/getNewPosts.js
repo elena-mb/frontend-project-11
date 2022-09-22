46538dece26de2830653abec07649a4c1749ecd5
Elena Balab <elena.mb.711@gmail.com>
2022-09-22 15:55:21 +0300

    Delete comments

--- src/getNewPosts.js
+++ src/getNewPosts.js
@@ -5,11 +5,5 @@ export default (url, lastUpdate) => fetchFeed(url)
   .then(({ contents }) => parseFeed(contents))
   .then(({ posts }) => {
     const newPosts = posts.filter(({ pubDate }) => pubDate > lastUpdate);
-    //   .map((post) => {
-    //     const id = uniqueId();
-    //     return { feedId, id, ...post };
-    //   });
-    // watchedState.posts.unshift(...newPosts);
-    // console.log(newPosts, 'from check');
     return newPosts;
   }).catch((e) => e);
