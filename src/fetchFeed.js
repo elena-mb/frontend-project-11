6a8c2a10560bd86ba5546d2ea5cb5516d28c853f
Elena Balab <elena.mb.711@gmail.com>
2022-09-22 21:01:38 +0300

    Fixing ...

--- src/fetchFeed.js
+++ src/fetchFeed.js
@@ -4,6 +4,7 @@ export default (uri) => (axios
   .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(uri)}`)
   .then(({ data }) => {
     const { contents, status: { content_type: contentType } } = data;
+    console.log(JSON.stringify(data.status));
     if (!contentType.includes('application/rss+xml')) {
       const error = {
         errors: [{ key: 'ERR_INVALID_RSS' }],
